acf1b8bc2f0493d0b55138df5ecb65edebd41692
IDGS-803-22002362 <81542@alumnos.utleon.edu.mx>
2025-01-30 22:59:15 -0600

    formularios

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app=Flask(__name__)
 @app.route('/')
 def index():
-    return "hola mami"
-@app.route('/hola')
-def hola():
-    return("hola")
+    lista={"betillo","betillo","antoi"}
+    grupo="IDGS-803"
+    #PARA LLEBVAR VARIABLE A FRONT inicializas la variable y la pones a un lado del archivo
+    return render_template("index.html",grupo=grupo,lista=lista)
+@app.route('/ejemplo1')
+def ejemplo1():
+    return render_template("ejemplo1.html")
+@app.route('/ejemplo2')
+def ejemplo2():
+    return render_template("ejemplo2.html")
+@app.route("/OperaBas")
+def Operas():
+    return render_template("OperaBas.html")
+
+@app.route("/Resultado", methods=["GET","POST"])
+def Resultado():
+    if request.method=="POST":
+        num1=request.form.get("N1")
+        num2=request.form.get("N2")
+        resultado=int(num2)+int(num1)
+        return render_template("OperaBas.html",resultado=resultado)
+
 @app.route("/user/<string:user>")
 def user(user):
     return f"hola {user}"
@@ -25,6 +43,15 @@ def suma(n1,n2):
 @app.route("/default/<string:nom>")
 def func(nom="pepe"):
     return "el nombre de Nom es"+nom
+
+@app.route("/form1")
+def form1():
+    return '''
+    <form>
+    <label>nombre</label>
+    <input type="text" name="nombre" placeholder="nombre">
+    </form>
+    '''
 
 if __name__=='__main__':
     app.run(debug=True)
